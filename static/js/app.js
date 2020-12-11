98f061bf60f9fa5a00a5ad5a68d78d0507883cdd
Sadikchhya Khatiwada <sadikchhya1@gmail.com>
2020-12-10 20:50:37 -0700

    Project files updated

---
+++ static/js/app.js
@@ -0,0 +1,25 @@
+
+var tbody = d3.select('tbody');
+var input = d3.select('input');
+var button = d3.select('button');
+
+showData(data);
+
+function showData(data) {
+    tbody.html('');
+    data.forEach(obj => {
+        var row = tbody.append('tr');
+        Object.values(obj).forEach(val => {
+            var cell = row.append('td');
+            cell.text(val)
+        })
+    });
+};
+
+button.on('click', () => {
+    var date = input.property('value');
+
+    var filterData = data.filter(obj => obj.datetime == date);
+
+    showData(filterData);
+})
